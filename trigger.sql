1f49eb3ff68b582ee950bda26b6658f900e66e12
gsc_prukol <gsc.prukol@gmail.com>
2018-12-04 23:26:59 +0200

    Grouped triggers by appointment.

--- trigger.sql
+++ trigger.sql
@@ -5,42 +5,28 @@ CREATE TRIGGER video_AFTER_INSERT AFTER INSERT ON Videos
 FOR EACH ROW
 BEGIN
 INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
-VALUES('Video', 'i', NEW.idVideo, NEW.name);
-END //
-
-CREATE TRIGGER video_BEFORE_UPDATE BEFORE UPDATE ON Videos 
-FOR EACH ROW
-BEGIN
-SET NEW.idVideo = OLD.idVideo;
+VALUES('Videos', 'i', NEW.idVideo, NEW.name);
 END //
 
 CREATE TRIGGER video_AFTER_UPDATE AFTER UPDATE ON Videos 
 FOR EACH ROW
 BEGIN
 INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
-VALUES('Video', 'u', NEW.idVideo, NEW.name);
+VALUES('Videos', 'u', NEW.idVideo, NEW.name);
 END //
 
 CREATE TRIGGER video_AFTER_DELETE AFTER DELETE ON Videos
 FOR EACH ROW
 BEGIN
 INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
-VALUES('Video', 'd', OLD.idVideo, OLD.name);
+VALUES('Videos', 'd', OLD.idVideo, OLD.name);
 END //
-
-
 
 CREATE TRIGGER user_AFTER_INSERT AFTER INSERT ON Users
 FOR EACH ROW
 BEGIN
 INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
 VALUES('Users', 'i', NEW.idUsers, NEW.login);
-END //
-
-CREATE TRIGGER user_BEFORE_UPDATE BEFORE UPDATE ON Users
-FOR EACH ROW
-BEGIN
-SET NEW.idUsers = OLD.idUsers;
 END //
 
 CREATE TRIGGER user_AFTER_UPDATE AFTER UPDATE ON Users
@@ -57,19 +43,11 @@ INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
 VALUES('Users', 'd', OLD.idUsers, OLD.login);
 END //
 
-
-
 CREATE TRIGGER group_AFTER_INSERT AFTER INSERT ON Groups
 FOR EACH ROW
 BEGIN
 INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
 VALUES('Groups', 'i', NEW.idGroups, NEW.name);
-END //
-
-CREATE TRIGGER group_BEFORE_UPDATE BEFORE UPDATE ON Groups
-FOR EACH ROW
-BEGIN
-SET NEW.idGroups = OLD.idGroups;
 END //
 
 CREATE TRIGGER group_AFTER_UPDATE AFTER UPDATE ON Groups
@@ -85,3 +63,25 @@ BEGIN
 INSERT INTO Log(tableName, actionType,idRowsTable,  logMessage)
 VALUES('Groups', 'd', OLD.idGroups, OLD.name);
 END //
+
+
+
+CREATE TRIGGER group_BEFORE_UPDATE BEFORE UPDATE ON Groups
+FOR EACH ROW
+BEGIN
+SET NEW.idGroups = OLD.idGroups;
+END //
+
+CREATE TRIGGER user_BEFORE_UPDATE BEFORE UPDATE ON Users
+FOR EACH ROW
+BEGIN
+SET NEW.idUsers = OLD.idUsers;
+END //
+
+CREATE TRIGGER video_BEFORE_UPDATE BEFORE UPDATE ON Videos 
+FOR EACH ROW
+BEGIN
+SET NEW.idVideo = OLD.idVideo;
+END //
+
+DELIMITER ;
