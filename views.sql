c365cb9b0828494f06743cbe3140683995818a91
gsc_prukol <gsc.prukol@gmail.com>
2018-12-10 22:30:50 +0200

    Добавив кілька в'юшок для відео.

---
+++ views.sql
@@ -0,0 +1,24 @@
+USE kursova2;
+CREATE VIEW top5 AS 
+    SELECT v.name AS name , count(*) AS count 
+        FROM Videos v 
+        OIN usersHasVideo uHV 
+        USING(idVideo) 
+        GROUP BY uHV.idVideo 
+        ORDER BY count DESC
+        LIMIT 5;
+
+CREATE VIEW top50 AS 
+    SELECT v.name AS name , count(*) AS count 
+        FROM Videos v 
+        OIN usersHasVideo uHV 
+        USING(idVideo) 
+        GROUP BY uHV.idVideo 
+        ORDER BY count DESC
+        LIMIT 50;
+
+CREATE VIEW rand5 AS 
+    SELECT v.name AS name
+        FROM Videos v
+        ORDER BY rand() 
+        LIMIT 5;
