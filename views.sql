e6c090bfe2cec534e1304a8ab6f28cd07643dd68
gsc_prukol <gsc.prukol@gmail.com>
2018-12-11 19:43:56 +0200

    Not drop if not exist

--- views.sql
+++ views.sql
@@ -1,6 +1,10 @@
 USE ka66_03;
+DROP VIEW IF EXISTS top5;
+DROP VIEW IF EXISTS top50;
+DROP VIEW IF EXISTS rand5;
+
 CREATE VIEW top5 AS 
-    SELECT v.name AS name , count(*) AS count 
+    SELECT v.name AS name , count(*) AS count, v.url as url 
         FROM Videos v 
         JOIN UsersHasVideo uHV 
         USING(idVideo) 
@@ -9,7 +13,7 @@ CREATE VIEW top5 AS
         LIMIT 5;
 
 CREATE VIEW top50 AS 
-    SELECT v.name AS name , count(*) AS count 
+    SELECT v.name AS name , count(*) AS count, v.url as url 
         FROM Videos v 
         JOIN UsersHasVideo uHV 
         USING(idVideo) 
@@ -18,7 +22,7 @@ CREATE VIEW top50 AS
         LIMIT 50;
 
 CREATE VIEW rand5 AS 
-    SELECT v.name AS name
+    SELECT v.name AS name, v.url as url 
         FROM Videos v
         ORDER BY rand() 
         LIMIT 5;
