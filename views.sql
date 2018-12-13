886e9edfb23e83991a8391f2e58fd2dc3383ac3c
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 00:09:59 +0200

    Перейменував 2

--- views.sql
+++ views.sql
@@ -5,24 +5,24 @@ DROP VIEW IF EXISTS rand5;
 
 CREATE VIEW top5 AS 
     SELECT v.name AS name , count(*) AS count, v.url as url 
-        FROM Videos v 
-        JOIN UsersHasVideo uHV 
-        USING(idVideo) 
-        GROUP BY uHV.idVideo 
+        FROM videos v 
+        JOIN users_has_video uHV 
+        USING(id_video) 
+        GROUP BY uHV.id_video 
         ORDER BY count DESC
         LIMIT 5;
 
 CREATE VIEW top50 AS 
     SELECT v.name AS name , count(*) AS count, v.url as url 
-        FROM Videos v 
-        JOIN UsersHasVideo uHV 
-        USING(idVideo) 
-        GROUP BY uHV.idVideo 
+        FROM videos v 
+        JOIN users_has_video uHV 
+        USING(id_video) 
+        GROUP BY uHV.id_video 
         ORDER BY count DESC
         LIMIT 50;
 
 CREATE VIEW rand5 AS 
     SELECT v.name AS name, v.url as url 
-        FROM Videos v
+        FROM videos v
         ORDER BY rand() 
         LIMIT 5;
