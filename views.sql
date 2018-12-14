0be17e50a2121aab5f29595d86edae719a42a574
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 20:12:26 +0200

    view edit

--- views.sql
+++ views.sql
@@ -2,7 +2,9 @@ USE ka66_03;
 DROP VIEW IF EXISTS top5;
 DROP VIEW IF EXISTS top50;
 DROP VIEW IF EXISTS rand5;
-
+DROP VIEW IF EXISTS rand50;
+DROP VIEW IF EXISTS group50;
+DROP VIEW IF EXISTS video50;
 CREATE VIEW top5 AS 
     SELECT v.name AS name , count(*) AS count, v.url as url 
         FROM videos v 
@@ -13,7 +15,7 @@ CREATE VIEW top5 AS
         LIMIT 5;
 
 CREATE VIEW top50 AS 
-    SELECT v.name AS name , count(*) AS count, v.url as url 
+    SELECT v.name AS name , count(*) AS count, v.url as url, v.id_video as id_video
         FROM videos v 
         JOIN user_has_video uHV 
         USING(id_video) 
@@ -28,19 +30,19 @@ CREATE VIEW rand5 AS
         LIMIT 5;
 
 CREATE VIEW rand50 AS 
-    SELECT v.name AS name, v.url as url 
+    SELECT v.name AS name, v.url as url, v.id_video as id_video 
         FROM videos v
         ORDER BY rand() 
         LIMIT 50;
 
-    CREATE VIEW group50 AS 
+CREATE VIEW group50 AS 
     SELECT g.name AS name, g.id_group as id_group 
         FROM groups g
         ORDER BY rand() 
         LIMIT 50;
 
 CREATE VIEW video50 AS 
-    SELECT v.name AS name, v.url as url 
+    SELECT v.name AS name, v.url as url, v.id_video as id_video 
         FROM videos v
         ORDER BY id_video DESC
         LIMIT 50;
